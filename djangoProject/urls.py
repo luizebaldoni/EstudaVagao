b1dbfe2c715cd879a4ecb465bd84bb07f7778892
VitorDallabona <vitor.contact.dallabona@gmail.com>
2023-11-05 18:07:39 -0300

    Alteração no banco de dados

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -4,9 +4,9 @@ ROTAS DEVEM SER ALTERADAS NO ARQUIVO URLS.PY NO DIRETORIO DJANGO_APP
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django_app import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-	path('django_app/', include('django_app.urls')),
+    path("login/", views.index, name="login"),
+	path('login/register/', views.register_user, name='register'),
 ]
