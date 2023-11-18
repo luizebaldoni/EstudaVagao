2767e3a0d731dd899281dda5dd6250e389c8bde6
luizebaldoni <oliveira.luize@acad.ufsm.br>
2023-11-18 18:25:14 -0300

    atualizado views.py

--- django_app/views.py
+++ django_app/views.py
@@ -2,29 +2,55 @@
 ARQUIVO PARA DEFINIR OS TEMPLATES DA APLICAÇAO E OQ FAZEMOS COM ELES
 '''
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegistration
 from .models import Users
+from django.conf import settings
+from . import forms
 
+# DEFINIÇÃO VIEW INICIAL (tela de login ou registro)
+def Login(request):
+    if not request.user.is_authenticated:
+        return render(request, template_name='registration/login.html')
+    return render(request, 'home.html')
 
-
-def Login(request_iter):
-    return render(request_iter, 'registration/login.html')
-
+# DEFINIÇÃO VIEW HOME (apos fzr login ou register)
 def home(request):
     return render(request, 'home.html')
 
-def register_user(request):
-    if request.method == "POST": # Se o usuário prencheu o formulário:
-        form = UserRegistration(request.POST)
+# DEFINIÇÃO VIEW DE LOGIN -- AINDA NÃO ESTÁ REDIRECIONANDO
+def user_login(request):
+    form = forms.UserRegistration()
+    message = ''
+    if request.method == 'POST':
+        form = forms.UserRegistration(request.GET)
         if form.is_valid():
-            form.save()
-            login(request, Users)
-            messages.success(request, ('Registrado com sucesso'))
-            return redirect('register.html')
-    else:
+            username = form.cleaned_data['nome']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'home.html')
+            else:
+                message = "O login falhou!"
+    return render(request, 'registration/login.html', context={'form': form, 'message': message})
+
+# DEFINIÇÃO VIEW DE REGISTRO DO USUÁRIO -- AINDA NÃO ESTÁ REDIRECIONANDO
+def register_user(request):
+    if request.method == 'POST':
         form = UserRegistration()
-        
-    return render(request, 'register.html', {'form':form})
+        return render(request, 'register.html', {'form': form})   # ql view usamos para fzr a aquisição 
+    if request.method == 'GET': # caso encontre um método GET:
+        form = UserRegistration(request.POST) 
+        if form.is_valid(): #se o formulario é valido
+            form.save() # salva as informaçoes
+            messages.success(request, 'Você se registrou com sucesso.')
+            login(request, form)
+            return render(request, 'registration/login.html') 
+        else:
+            return render(request, 'register.html', {'form': form}) # caso contrario apresenta o forms novamente
