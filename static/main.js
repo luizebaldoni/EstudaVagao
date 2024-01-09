60f00a88c2610a05de23b00415a3f9fb638f0279
luizebaldoni <oliveira.luize@acad.ufsm.br>
2024-01-09 15:50:39 -0300

    alteração js

--- static/main.js
+++ static/main.js
@@ -18,33 +18,6 @@ $(function(){
 	   }
 	});
   });
-
-// --------------add active class-on another-page move----------
-jQuery(document).ready(function($){
-	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
-	// Account for home page with empty path
-	if ( path == '' ) {
-		path = 'index.html';
-	}
-	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-	// Add active class to target link
-	target.parent().addClass('active');
-});
-function hideIconBar(){
-    var iconBar = document.getElementById("iconBar");
-    var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:none;");
-    navigation.classList.remove("hide");
-}
-
-function showIconBar(){
-    var iconBar = document.getElementById("iconBar");
-    var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:block;");
-    navigation.classList.add("hide");
-}
-
 // Comentários
 function showComment(){
     var commentArea = document.getElementById("comment-area");
