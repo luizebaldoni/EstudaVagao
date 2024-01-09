85b14c5af5c3d7901a5f121ad9bfad54d7d2cb9a
luizebaldoni <oliveira.luize@acad.ufsm.br>
2024-01-09 14:53:27 -0300

    pagina home (base.html) finalizada

--- app/forms.py
+++ app/forms.py
@@ -5,99 +5,17 @@ from django import forms
 from tinymce.widgets import TinyMCE 
 from ckeditor.widgets import CKEditorWidget
 
-# FORMULARIO DE REGISTRO
-''''
-from django.contrib import admin
-from django import forms
-from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.exceptions import ValidationError
 
-from app.models import User
-
-class UserCreationForm(forms.ModelForm):
-    """formulário para criar novos usuários. Inclui todos os campos
-    requisitado mais uma senha repetida"""
-
-    password1 = forms.CharField(label="Senha", widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label="Confirmação de senha", widget=forms.PasswordInput
-    )
-
-    class Meta:
-        model = User
-        fields = ["email", "username", "matricula", "ano"]
-
-    def clean_password2(self):
-        # Checa se as duas entradas para a senha são idênticas
-        
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("As senhas disponibilizadas são diferentes")
-        return password2
-
-    def save(self, commit=True):
-        # Salva a senha no formado de hash
-        
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-class UserChangeForm(forms.ModelForm):
-
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = User
-        fields = ["email", "password", "username", "ano", "matricula", "is_active", "is_admin"]
-
-class UserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
-    form = UserChangeForm
-    add_form = UserCreationForm
-
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    list_display = ["email", "username", "ano", "matricula", "is_admin"]
-    list_filter = ["is_admin"]
-    fieldsets = [
-        (None, {"fields": ["username", "password"]}),
-        ("Personal info", {"fields": ["ano", "matricula"]}),
-        ("Permissions", {"fields": ["is_admin"]}),
-    ]
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
-    add_fieldsets = [
-        (
-            None,
-            {
-                "classes": ["wide"],
-                "fields": ["email", "ano", "matricula", "username", "password1", "password2"],
-            },
-        ),
-    ]
-    search_fields = ["username"]
-    ordering = ["username"]
-    filter_horizontal = []
-
-
-'''
 class TinyMCEWidget(TinyMCE): 
     def use_required_attribute(self, *args): 
         return False
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorWidget())
-    
+    content = HTMLField()
     content = forms.CharField(
         widget=TinyMCEWidget( 
-            attrs={'required': False, 'cols': 30, 'rows': 10}), 
+            attrs={'required': False, 'cols': 10, 'rows': 5}), 
             label = "Sua pergunta", 
     )
     
