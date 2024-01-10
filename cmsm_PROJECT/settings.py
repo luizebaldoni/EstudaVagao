704f16a0f4efe9ab9925c1038ea86f584dd5fe03
luizebaldoni <oliveira.luize@acad.ufsm.br>
2024-01-10 20:39:18 -0300

    correção de media

--- cmsm_PROJECT/settings.py
+++ cmsm_PROJECT/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
 MEDIA_ROOT  = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
--- register/urls.py
+++ register/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from cmsm_PROJECT import settings
 from .views import *
 
 urlpatterns = [
@@ -6,4 +9,4 @@ urlpatterns = [
     path("", signin, name="signin"),
     path("update_profile/", update_profile, name="update"),
     path("logout/", logout, name="logout"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
